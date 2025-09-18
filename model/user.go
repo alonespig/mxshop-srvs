6c202f3329244e4db257ec2aeea96729efb0b10f
alone pig <3461266094@qq.com>
2025-09-18 16:17:34 +0800

    1-2 同步表结构

--- model/user.go
+++ model/user.go
@@ -16,7 +16,10 @@ type BaseModel struct {
 
 type User struct {
 	BaseModel
-	Mobile   string `gorm:"index:idx_mobile;unique;type:varchar(11);not null"`
-	Password string `gorm:"type:varchar(100);not null"`
-	NickName string `gorm:"type:varchar(20)"`
+	Mobile   string     `gorm:"index:idx_mobile;unique;type:varchar(11);not null"`
+	Password string     `gorm:"type:varchar(100);not null"`
+	NickName string     `gorm:"type:varchar(20)"`
+	Birthday *time.Time `gorm:"type:datetime"`
+	Gender   string     `gorm:"column:gender;default:male;type:varchar(10) comment 'male 男, female 女'"`
+	Role     int        `gorm:"column:role;default:1;type:int comment '1 普通用户, 2 管理员'"`
 }
