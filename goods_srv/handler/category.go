66ced71caead41a4562894dbd29200657dd15627
alone pig <3461266094@qq.com>
2025-09-23 20:52:49 +0800

    获取商品分类的子分类

--- goods_srv/handler/category.go
+++ goods_srv/handler/category.go
@@ -8,6 +8,8 @@ import (
 	"mxshop/proto"
 
 	"github.com/golang/protobuf/ptypes/empty"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // 商品分类
@@ -23,7 +25,42 @@ func (g *GoodsServer) GetAllCategorysList(ctx context.Context, req *empty.Empty)
 
 // 获取子分类
 func (g *GoodsServer) GetSubCategory(ctx context.Context, req *proto.CategoryListRequest) (*proto.SubCategoryListResponse, error) {
-	return nil, nil
+	var category model.Category
+	if result := global.DB.First(&category, req.Id); result.RowsAffected == 0 {
+		return nil, status.Errorf(codes.NotFound, "分类不存在")
+	}
+
+	categoryListResponse := proto.SubCategoryListResponse{}
+	categoryListResponse.Info = &proto.CategoryInfoResponse{
+		Id:             category.ID,
+		Name:           category.Name,
+		Level:          category.Level,
+		IsTab:          category.IsTab,
+		ParentCategory: category.ParentCategoryID,
+	}
+
+	var subCategorys []model.Category
+	preloads := "SubCategory"
+	if category.Level == 1 {
+		preloads = "SubCategory.SubCategory"
+	}
+
+	global.DB.Where(&model.Category{ParentCategoryID: category.ID}).Preload(preloads).Find(&subCategorys)
+
+	var subCategorysResponse []*proto.CategoryInfoResponse
+	for _, subCategory := range subCategorys {
+		subCategorysResponse = append(subCategorysResponse, &proto.CategoryInfoResponse{
+			Id:             subCategory.ID,
+			Name:           subCategory.Name,
+			Level:          subCategory.Level,
+			IsTab:          subCategory.IsTab,
+			ParentCategory: subCategory.ParentCategoryID,
+		})
+	}
+
+	categoryListResponse.SubCategorys = subCategorysResponse
+
+	return &categoryListResponse, nil
 }
 func (g *GoodsServer) CreateCategory(ctx context.Context, req *proto.CategoryInfoRequest) (*proto.CategoryInfoResponse, error) {
 	return nil, nil
