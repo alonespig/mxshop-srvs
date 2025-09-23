485f4dd8599937ec3138b79d61b81bc2ac17a3fa
alone pig <3461266094@qq.com>
2025-09-23 12:00:03 +0800

    server 集成nacos

--- user_srv/initialize/config.go
+++ user_srv/initialize/config.go
@@ -1,10 +1,15 @@
 package initialize
 
 import (
+	"encoding/json"
 	"fmt"
 	"mxshop/global"
 
+	"github.com/nacos-group/nacos-sdk-go/clients"
+	"github.com/nacos-group/nacos-sdk-go/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/vo"
 	"github.com/spf13/viper"
+	"go.uber.org/zap"
 )
 
 func GetEnvInfo(env string) string {
@@ -21,10 +26,74 @@ func InitConfig() {
 	}
 	v := viper.New()
 	v.SetConfigFile(configFileName)
+
 	if err := v.ReadInConfig(); err != nil {
 		panic(err)
 	}
-	if err := v.Unmarshal(&global.ServerConfig); err != nil {
+
+	if err := v.Unmarshal(&global.NacosConfig); err != nil {
+		panic(err)
+	}
+
+	zap.L().Info("配置文件加载成功", zap.Any("NacosConfig", global.NacosConfig))
+
+	sc := []constant.ServerConfig{
+		{
+			IpAddr: global.NacosConfig.Host,
+			Port:   global.NacosConfig.Port,
+		},
+	}
+
+	cc := constant.ClientConfig{
+		NamespaceId:         global.NacosConfig.Namespace,
+		TimeoutMs:           5000,
+		NotLoadCacheAtStart: true,
+		LogDir:              "tmp/nacos/log",
+		CacheDir:            "tmp/nacos/cache",
+		LogLevel:            "debug",
+	}
+
+	configClient, err := clients.CreateConfigClient(map[string]interface{}{
+		"serverConfigs": sc,
+		"clientConfig":  cc,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	content, err := configClient.GetConfig(vo.ConfigParam{
+		DataId: global.NacosConfig.DataId,
+		Group:  global.NacosConfig.Group,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(content)
+
+	err = json.Unmarshal([]byte(content), &global.ServerConfig)
+	if err != nil {
+		zap.L().Fatal("读取配置文件失败", zap.Error(err))
+	}
+
+	fmt.Println(global.ServerConfig)
+
+	err = configClient.ListenConfig(vo.ConfigParam{
+		DataId: global.NacosConfig.DataId,
+		Group:  global.NacosConfig.Group,
+		OnChange: func(namespace string, group string, dataId string, data string) {
+			fmt.Println("配置文件发生变化")
+			fmt.Println("gropu ", group, "dataId ", dataId, "data ", data)
+			err = json.Unmarshal([]byte(data), &global.ServerConfig)
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Println(global.ServerConfig)
+		},
+	})
+	if err != nil {
+		zap.L().Fatal("监听配置文件失败", zap.Error(err))
 		panic(err)
 	}
 }
