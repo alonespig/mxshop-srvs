8e254bd8365c2195b73b2f5a1d0db498b5d3ffc5
alone pig <3461266094@qq.com>
2025-09-23 12:51:35 +0800

    品牌，轮播图表

--- good_srv/model/goods.go
+++ good_srv/model/goods.go
@@ -8,3 +8,32 @@ type Category struct {
 	ParentCategoryID int32     `gorm:"type:int"`
 	ParentCategory   *Category `gorm:"foreignKey:ParentCategoryID"`
 }
+
+type Brands struct {
+	BaseModel
+	Name string `gorm:"type:varchar(20);not null"`
+	Logo string `gorm:"type:varchar(200);default:'';not null"`
+}
+
+type GoodsCategoryBrand struct {
+	BaseModel
+	CategoryID int32 `gorm:"type:int;index:idx_category_brand,unique"`
+	Category   Category
+	BrandID    int32 `gorm:"type:int;index:idx_category_brand,unique"`
+	Brands     Brands
+}
+
+func (GoodsCategoryBrand) TableName() string {
+	return "goodscategorybrand"
+}
+
+type Banner struct {
+	BaseModel
+	Image string `gorm:"type:varchar(200);not null"`
+	Url   string `gorm:"type:varchar(200);not null"`
+	Index int32  `gorm:"type:int;not null;default:1"`
+}
+
+func (Banner) TableName() string {
+	return "banner"
+}
