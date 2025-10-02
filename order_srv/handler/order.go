81b88ac21ef08253096896457f83afaab961dc10
alone pig <3461266094@qq.com>
2025-10-02 16:32:09 +0800

    完成订单服务接口

--- order_srv/handler/order.go
+++ order_srv/handler/order.go
@@ -2,9 +2,14 @@ package handler
 
 import (
 	"context"
+	"fmt"
+	"math/rand"
 	"mxshop/global"
 	"mxshop/model"
 	"mxshop/proto"
+	"time"
+
+	_ "github.com/mbobakov/grpc-consul-resolver"
 
 	"github.com/golang/protobuf/ptypes/empty"
 	"google.golang.org/grpc/codes"
@@ -86,6 +91,11 @@ func (s *OrderServer) UpdateCartItem(ctx context.Context, req *proto.CartItemReq
 	return &empty.Empty{}, nil
 }
 
+func generateOrderSn(userId int32) string {
+	rand.Seed(time.Now().UnixNano())
+	return fmt.Sprintf("%d%d%d%d%d%d%d%d", userId, time.Now().Year(), time.Now().Month(), time.Now().Day(), time.Now().Hour(), time.Now().Minute(), time.Now().Second(), rand.Intn(90)+10)
+}
+
 func (s *OrderServer) CreateOrder(ctx context.Context, req *proto.OrderRequest) (*proto.OrderInfoResponse, error) {
 	//新建订单 访问商品微服务获取商品信息
 	//库存的扣减 访问库存微服务
@@ -97,11 +107,82 @@ func (s *OrderServer) CreateOrder(ctx context.Context, req *proto.OrderRequest) 
 		return nil, status.Error(codes.InvalidArgument, "没有选中结算的商品")
 	}
 
-	for _, shopCarts := range shopCarts {
-		goodsIds = append(goodsIds, shopCarts.Goods)
+	goodsNumMap := make(map[int32]int32)
+	for _, shopCart := range shopCarts {
+		goodsIds = append(goodsIds, shopCart.Goods)
+		goodsNumMap[shopCart.Goods] = shopCart.Nums
 	}
 
-	return nil, nil
+	//跨服务调用商品 -gin
+	goods, err := global.GoodsSrvClient.BatchGetGoods(ctx, &proto.BatchGoodsIdInfo{Id: goodsIds})
+	if err != nil {
+		return nil, status.Error(codes.Internal, "批量查询商品信息失败")
+	}
+
+	var orderAmount float32
+	var orderGoods []*model.OrderGoods
+	var goodsInvInfo []*proto.GoodsInvInfo
+
+	for _, good := range goods.Data {
+		orderAmount += good.ShopPrice * float32(goodsNumMap[good.Id])
+		orderGoods = append(orderGoods, &model.OrderGoods{
+			Goods:      good.Id,
+			GoodsName:  good.Name,
+			GoodsImage: good.GoodsFrontImage,
+			GoodsPrice: good.ShopPrice,
+			Nums:       goodsNumMap[good.Id],
+		})
+		goodsInvInfo = append(goodsInvInfo, &proto.GoodsInvInfo{
+			GoodsId: good.Id,
+			Num:     goodsNumMap[good.Id],
+		})
+	}
+
+	//跨服务调用库存微服务进行库存扣减
+	_, err = global.InventorySrvClient.Sell(ctx, &proto.SellInfo{GoodsInfo: goodsInvInfo})
+	if err != nil {
+		return nil, status.Error(codes.Internal, "库存扣减失败")
+	}
+
+	tx := global.DB.Begin()
+	//生成订单表
+	order := model.OrderInfo{
+		OrderSn:      generateOrderSn(req.UserId),
+		OrderMount:   orderAmount,
+		Address:      req.Address,
+		SignerName:   req.Name,
+		SingerMobile: req.Mobile,
+		Post:         req.Post,
+		User:         req.UserId,
+	}
+
+	if err := tx.Save(&order).Error; err != nil {
+		tx.Rollback()
+		return nil, status.Error(codes.Internal, "生成订单失败")
+	}
+
+	for _, orderGood := range orderGoods {
+		orderGood.Order = order.ID
+	}
+
+	//批量插入 orderGoods
+	if err := tx.CreateInBatches(orderGoods, 100).Error; err != nil {
+		tx.Rollback()
+		return nil, status.Error(codes.Internal, "生成订单失败")
+	}
+
+	if err := tx.Where(&model.ShoppingCart{User: req.UserId, Checked: true}).Delete(&model.ShoppingCart{}).Error; err != nil {
+		tx.Rollback()
+		return nil, status.Error(codes.Internal, "生成订单失败")
+	}
+
+	tx.Commit()
+
+	return &proto.OrderInfoResponse{
+		Id:      order.ID,
+		OrderSn: order.OrderSn,
+		Total:   order.OrderMount,
+	}, nil
 }
 
 func (s *OrderServer) OrderDetail(ctx context.Context, req *proto.OrderRequest) (*proto.OrderInfoDetailResponse, error) {
@@ -155,7 +236,7 @@ func (s *OrderServer) OrderList(ctx context.Context, req *proto.OrderFilterReque
 	rsp := proto.OrderListResponse{}
 	rsp.Total = int32(total)
 
-	result = global.DB.Scopes(Paginate(int(req.Pages), int(req.PagePerNums))).Find(&orders)
+	result = global.DB.Scopes(Paginate(int(req.Pages), int(req.PagePerNums))).Where(&model.OrderInfo{User: req.UserId}).Find(&orders)
 	if result.Error != nil {
 		return nil, result.Error
 	}
@@ -178,5 +259,8 @@ func (s *OrderServer) OrderList(ctx context.Context, req *proto.OrderFilterReque
 }
 
 func (s *OrderServer) UpdateOrderStatus(ctx context.Context, req *proto.OrderStatus) (*empty.Empty, error) {
-	return nil, nil
+	if result := global.DB.Model(&model.OrderInfo{}).Where("order_sn = ?", req.Id, req.OrderSn).Update("status", req.Status); result.RowsAffected == 0 {
+		return nil, status.Errorf(codes.NotFound, "订单不存在")
+	}
+	return &empty.Empty{}, nil
 }
