b56196d638181c04822c155c99dead46fc08df3e
alone pig <3461266094@qq.com>
2025-10-03 14:36:05 +0800

    完成创建订单

--- mxshop-api/order-web/api/order/order.go
+++ mxshop-api/order-web/api/order/order.go
@@ -2,6 +2,7 @@ package order
 
 import (
 	"mxshop-api/order-web/api"
+	"mxshop-api/order-web/forms"
 	"mxshop-api/order-web/global"
 	"mxshop-api/order-web/models"
 	"mxshop-api/order-web/proto"
@@ -67,9 +68,87 @@ func List(ctx *gin.Context) {
 }
 
 func New(ctx *gin.Context) {
+	orderForm := forms.CreateOrderForm{}
+	if err := ctx.ShouldBind(&orderForm); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"msg": err.Error(),
+		})
+		return
+	}
 
+	userId, _ := ctx.Get("userId")
+	rsp, err := global.OrderSrvClient.CreateOrder(ctx, &proto.OrderRequest{
+		UserId:  int32(userId.(uint)),
+		Address: orderForm.Address,
+		Mobile:  orderForm.Mobile,
+		Name:    orderForm.Name,
+		Post:    orderForm.Post,
+	})
+
+	if err != nil {
+		zap.S().Error("创建订单失败", err)
+		api.HandleGrpcErrorToHttp(err, ctx)
+		return
+	}
+
+	//TODO 返回支付宝到的url
+	ctx.JSON(http.StatusOK, gin.H{
+		"id": rsp.Id,
+	})
 }
 
 func Detail(ctx *gin.Context) {
+	id := ctx.Param("id")
+	userId, _ := ctx.Get("userId")
+	i, err := strconv.ParseInt(id, 10, 32)
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"msg": "url格式错误",
+		})
+		return
+	}
 
+	request := proto.OrderRequest{
+		Id: int32(i),
+	}
+	claims, _ := ctx.Get("claims")
+	model := claims.(models.CustomClaims)
+	if model.AuthorityId == 1 {
+		request.UserId = int32(userId.(uint))
+	}
+
+	rsp, err := global.OrderSrvClient.OrderDetail(ctx, &request)
+	if err != nil {
+		zap.S().Error("查询订单详情失败", err)
+		api.HandleGrpcErrorToHttp(err, ctx)
+		return
+	}
+
+	reMap := gin.H{
+		"id":       rsp.OrderInfo.Id,
+		"status":   rsp.OrderInfo.Status,
+		"user":     rsp.OrderInfo.UserId,
+		"post":     rsp.OrderInfo.Post,
+		"total":    rsp.OrderInfo.Total,
+		"address":  rsp.OrderInfo.Address,
+		"name":     rsp.OrderInfo.Name,
+		"mobile":   rsp.OrderInfo.Mobile,
+		"pay_type": rsp.OrderInfo.PayType,
+		"order_sn": rsp.OrderInfo.OrderSn,
+	}
+
+	goodsList := make([]interface{}, 0)
+	for _, item := range rsp.Goods {
+		goodsList = append(goodsList, map[string]interface{}{
+			"id":    item.Id,
+			"name":  item.GoodsName,
+			"price": item.GoodsPrice,
+			"image": item.GoodsImage,
+			"nums":  item.Nums,
+		})
+	}
+
+	reMap["goods"] = goodsList
+
+	ctx.JSON(http.StatusOK, reMap)
 }
--- mxshop-api/order-web/forms/order.go
+++ mxshop-api/order-web/forms/order.go
@@ -1,1 +1,8 @@
 package forms
+
+type CreateOrderForm struct {
+	Address string `form:"address" json:"address" binding:"required"`
+	Mobile  string `form:"mobile" json:"mobile" binding:"required"`
+	Name    string `form:"name" json:"name" binding:"required"`
+	Post    string `form:"post" json:"post" binding:"required"`
+}
